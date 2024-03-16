3da4b1f7744bbef343effc910f077b458de3afef
Mello Birkan <mgbirkan@icloud.com>
2024-03-16 12:37:15 -0300

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Marcello Gonzatto Birkan on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
