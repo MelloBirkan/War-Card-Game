8a20c1f514462812f62d0da79c5b5e4d370b8b78
Mello Birkan <mgbirkan@icloud.com>
2024-03-16 12:55:38 -0300

    UI Feita

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -9,16 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+          Image(.backgroundPlain)
+          
+          VStack(spacing: 30, content: {
+            Image(.logo)
+            
+            HStack(spacing: 40, content: {
+              Image(.card14)
+              
+              Image(.card10)
+            })
+            
+            Button(action: {}, label: {
+              Image(.button)
+            })
+            
+            HStack(spacing: 120, content: {
+              statsView(player: "Player", points: 0)
+              statsView(player: "CPU", points: 0)
+            })
+          })
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+func statsView(player: String, points: Int) -> some View {
+  VStack(spacing: 15, content: {
+  Text(player)
+  
+  Text("\(points)")
+    .font(.title)
+})
+.foregroundStyle(.white)
 }
